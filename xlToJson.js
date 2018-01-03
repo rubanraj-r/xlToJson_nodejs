0e887e1f8e70f33cd1a4b53bf8c43979800d47cc
rbnrjR <rubanrajr@hotmail.com>
2018-01-03 11:47:27 +0530

    xlToJson made

---
+++ xlToJson.js
@@ -0,0 +1,31 @@
+const fs= require('fs')
+    , XLSX = require('xlsx');
+
+var workbook = XLSX.readFile('./par.xlsx');
+var sheet_name_list = workbook.SheetNames;
+sheet_name_list.forEach(function(y) {
+    var worksheet = workbook.Sheets[y];
+    var headers = {};
+    var data = [];
+    for(z in worksheet) {
+        if(z[0] === '!') continue;
+        //parse out the column, row, and value
+        var col = z.substring(0,1);
+        var row = parseInt(z.substring(1));
+        var value = worksheet[z].v;
+
+        //store header names
+        if(row == 1) {
+            headers[col] = value;
+            continue;
+        }
+
+        if(!data[row]) data[row]={};
+        data[row][headers[col]] = value;
+    }
+    //drop those first two rows which are empty
+    data.shift();
+    data.shift();
+    console.log(data);
+    fs.writeFileSync('./par.json', JSON.stringify(data, null, '\r\n'), 'UTF-8');
+});
