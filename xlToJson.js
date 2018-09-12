fac118feb51e2f347f27f41943e850840a9b0820
Rubanraj R (Digital) <RU353746@wipro.com>
2018-09-12 11:32:51 +0530

    converted to promises and modularized

--- xlToJson.js
+++ xlToJson.js
@@ -1,31 +1,42 @@
 const fs= require('fs')
     , XLSX = require('xlsx');
 
-var workbook = XLSX.readFile('./par.xlsx');
-var sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(function(y) {
-    var worksheet = workbook.Sheets[y];
-    var headers = {};
-    var data = [];
-    for(z in worksheet) {
-        if(z[0] === '!') continue;
-        //parse out the column, row, and value
-        var col = z.substring(0,1);
-        var row = parseInt(z.substring(1));
-        var value = worksheet[z].v;
+function xlToJson(xl, json) {
+  return new Promise((resolve, reject) => {
+    try {
+      var workbook = XLSX.readFile(xl);
+      var sheet_name_list = workbook.SheetNames;
+      sheet_name_list.map((y) => {
+        var worksheet = workbook.Sheets[y];
+        var headers = {};
+        var data = [];
+        for(z in worksheet) {
+          if(z[0] === '!') continue;
+          //parse out the column, row, and value
+          var col = z.substring(0,1);
+          var row = parseInt(z.substring(1));
+          var value = worksheet[z].v;
 
-        //store header names
-        if(row == 1) {
+          //store header names
+          if(row == 1) {
             headers[col] = value;
             continue;
-        }
+          }
 
-        if(!data[row]) data[row]={};
-        data[row][headers[col]] = value;
+          if(!data[row]) data[row]={};
+          data[row][headers[col]] = value;
+        }
+        //drop those first two rows which are empty
+        data.shift();
+        data.shift();
+        // console.log(data);
+        fs.writeFileSync(json, JSON.stringify(data, null, '\r\n'), 'UTF-8');
+        resolve(" `` Converted to JSON `` ");
+      });
+    } catch (e) {
+      reject(e);
     }
-    //drop those first two rows which are empty
-    data.shift();
-    data.shift();
-    console.log(data);
-    fs.writeFileSync('./par.json', JSON.stringify(data, null, '\r\n'), 'UTF-8');
-});
+  });
+};
+
+module.exports = xlToJson;
